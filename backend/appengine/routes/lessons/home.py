e0f42be1cbf6d254296711a3865430a80f615c5d
Beatriz Alves Fonseca <betrizaf@gmail.com>
2015-05-26 23:10:54 -0300

    Commit na branch de CRUD

--- backend/appengine/routes/lessons/home.py
+++ backend/appengine/routes/lessons/home.py
@@ -1,13 +1,12 @@
-
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
-from gaepermission.decorator import login_not_required, permissions
-from permission_app.model import ADMIN
-from routes.lessons.new import salvar
+from gaepermission.decorator import login_not_required
+from tekton.gae.middleware.redirect import RedirectResponse
 from tekton.router import to_path
-from tema.tema_model import Tema, Licao
+from tema.tema_model import Tema, Licao, LicaoForm
 
 __author__ = 'Bea'
 
@@ -16,9 +15,24 @@ __author__ = 'Bea'
 def index(tema_selecionado = None):
     ctx = {'temas':Tema.query_ordenada_por_titulo().fetch(),
            'salvar_path':to_path(salvar),
-           'pesquisar_path':to_path(index)}
+           'pesquisar_path:':to_path(index)}
     if tema_selecionado is None:
+        ctx['lessons'] = Licao.query_ordenada_por_titulo().fetch()
         ctx['tema_selecionado'] = None
     else:
         ctx['tema_selecionado'] = Tema.get_by_id(int(tema_selecionado))
+        ctx['lessons'] = Licao.query_por_tema_ordenada_por_nome(tema_selecionado)
+    return TemplateResponse(ctx,'lessons/home.html')
+
+@login_not_required
+@no_csrf
+def salvar(**propriedades):
+    propriedades['tema']=ndb.Key(Tema,int(propriedades['tema']))
+    form = LicaoForm(**propriedades)
+    erros = form.validate()
+    if not erros:
+        lesson = form.fill_model()
+        lesson.put()
+        return RedirectResponse(index)
+    ctx = {'tema':propriedades, 'erros':erros}
     return TemplateResponse(ctx,'lessons/home.html')
