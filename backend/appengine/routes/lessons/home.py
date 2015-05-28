ee3a22e684543ce8f6a771572b75e32afecdeb12
Beatriz Alves Fonseca <betrizaf@gmail.com>
2015-05-27 22:38:00 -0300

    listagem estática

--- backend/appengine/routes/lessons/home.py
+++ backend/appengine/routes/lessons/home.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from google.appengine.ext import ndb
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
-from gaepermission.decorator import login_not_required
+from gaepermission.decorator import login_not_required, permissions
+from permission_app.model import ADMIN
 from routes.lessons.new import salvar
-from tekton.gae.middleware.redirect import RedirectResponse
 from tekton.router import to_path
-from tema.tema_model import Tema, Licao, LicaoForm
+from tema.tema_model import Tema, Licao
 
 __author__ = 'Bea'
 
@@ -18,9 +17,18 @@ def index(tema_selecionado = None):
            'salvar_path':to_path(salvar),
            'pesquisar_path':to_path(index)}
     if tema_selecionado is None:
-        ctx['lessons'] = Licao.query_ordenada_por_titulo().fetch()
         ctx['tema_selecionado'] = None
     else:
         ctx['tema_selecionado'] = Tema.get_by_id(int(tema_selecionado))
-        ctx['lessons'] = Licao.query_por_tema_ordenada_por_nome(tema_selecionado)
     return TemplateResponse(ctx,'lessons/home.html')
+
+def pesquisar():
+    ctx = {'temas':Tema.query_ordenada_por_titulo().fetch(),
+           }
+    if tema_selecionado is None:
+         ctx['lessons'] = Licao.query_ordenada_por_titulo().fetch()
+         ctx['tema_selecionado'] = None
+    else:
+         ctx['tema_selecionado'] = Tema.get_by_id(int(tema_selecionado))
+         ctx['lessons'] = Licao.query_por_tema_ordenada_por_nome(tema_selecionado)
+-   return TemplateResponse(ctx,'lessons/home.html')
