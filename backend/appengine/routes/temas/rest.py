e20aeacb56de2e60b1f9cc21768f9a7fae1a55c7
Beatriz Alves Fonseca <betrizaf@gmail.com>
2015-06-04 18:20:47 -0300

    Remoção e Listagem de tema funcionando ^^

--- backend/appengine/routes/temas/rest.py
+++ backend/appengine/routes/temas/rest.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import json
+from google.appengine.ext import ndb
 from distutils import log
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 from tekton.gae.middleware.json_middleware import JsonUnsecureResponse, JsonResponse
-from tema.tema_model import LicaoForm, TemaForm
+from tema.tema_model import LicaoForm, TemaForm, Tema
 
 __author__ = 'Bea'
+
+@login_not_required
+@no_csrf
+def index():
+    query = Tema.query_ordenada_por_titulo()
+    temas = query.fetch()
+    for tema in temas:
+        key = tema.key
+        key_id = key.id()
+    form = TemaForm()
+    temas = [form.fill_with_model(t) for t in temas]
+
+    return JsonResponse(temas)
 
 @login_not_required
 @no_csrf
@@ -21,3 +36,9 @@ def salvar(_resp, **propriedades):
     dct = form.fill_with_model(tema)
     log.info(dct)
     return JsonUnsecureResponse(dct)
+
+@login_not_required
+@no_csrf
+def deletar(tema_id):
+    key = ndb.Key(Tema, int(tema_id))
+    key.delete()
--- backend/appengine/static/tema/js/tema.js
+++ backend/appengine/static/tema/js/tema.js
@@ -1,66 +1,61 @@
 /**
  * Created by Bea on 28/05/2015.
  */
-var temaModule = angular.module('temaModule',['rest']);
+var temaModule = angular.module('temaModule', ['rest']);
 
-temaModule.directive('temaform', function() {
-   return {
-       restrict: 'E',
-       replace: true,
-       templateUrl: '/static/tema/html/tema_form.html',
-       scope: {
-           tema: '=',
-           tituloLabel: '@',
-           descricaoLabel: '@'
-       },
-       controller: function($scope, TemaApi) {
-           $scope.savingFlag = false;
-           $scope.salvar = function() {
-               $scope.savingFlag = true;
-               $scope.erros = {};
-               var promisse = TemaApi.salvar($scope.tema);
-               promisse.success(function(tema) {
-                   console.log(tema);
-                   $scope.tema.titulo = '';
-                   $scope.tema.descricao = '';
-                   $scope.savingFlag = false;
-               })
-               promisse.error(function(erros) {
-                   $scope.erros = erros;
-                   console.log(erros);
-                   $scope.savingFlag = false;
-               });
-           }
-       }
-   };
+temaModule.directive('temaform', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/static/tema/html/tema_form.html',
+        scope: {
+            tema: '=',
+            tituloLabel: '@',
+            descricaoLabel: '@',
+            saveComplete: '&'
+        },
+        controller: function ($scope, TemaApi) {
+            $scope.savingFlag = false;
+            $scope.salvar = function () {
+                $scope.savingFlag = true;
+                $scope.erros = {};
+                var promisse = TemaApi.salvar($scope.tema);
+                promisse.success(function (tema) {
+                    console.log(tema);
+                    if ($scope.saveComplete != undefined) {
+                        $scope.saveComplete({'tema': tema});
+                    }
+                    $scope.tema.titulo = '';
+                    $scope.tema.descricao = '';
+                    $scope.savingFlag = false;
+                })
+                promisse.error(function (erros) {
+                    $scope.erros = erros;
+                    console.log(erros);
+                    $scope.savingFlag = false;
+                });
+            }
+        }
+    };
 });
 
-temaModule.directive('temalinha', function() {
-   return {
-       restrict: 'E',
-       replace: true,
-       templateUrl: '/static/tema/html/tema_linha_tabela.html',
-       scope: {
-           tema: '='
-       },
-       controller: function($scope, TemaApi) {
-           $scope.savingFlag = false;
-           $scope.salvar = function() {
-               $scope.savingFlag = true;
-               $scope.erros = {};
-               var promisse = TemaApi.salvar($scope.tema);
-               promisse.success(function(tema) {
-                   console.log(tema);
-                   $scope.tema.titulo = '';
-                   $scope.tema.descricao = '';
-                   $scope.savingFlag = false;
-               })
-               promisse.error(function(erros) {
-                   $scope.erros = erros;
-                   console.log(erros);
-                   $scope.savingFlag = false;
-               });
-           }
-       }
-   };
+temaModule.directive('temalinha', function () {
+    return {
+        restrict: 'A',
+        replace: true,
+        templateUrl: '/static/tema/html/tema_linha_tabela.html',
+        scope: {
+            tema: '=',
+            deleteComplete: '&'
+        },
+        controller: function ($scope, TemaApi) {
+            $scope.ajaxComplete = false;
+            $scope.deletar = function() {
+                $scope.ajaxComplete = true;
+                TemaApi.deletar($scope.tema.id).success(function() {
+                    $scope.deleteComplete({'tema':$scope.tema});
+                });
+            }
+        }
+    };
 });
