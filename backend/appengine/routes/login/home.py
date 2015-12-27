f5973533485c0b6c804c8edeceb96407a5ced292
beatorizu <betrizaf@gmail.com>
2015-12-27 01:32:36 -0200

    Implementing Facebook and Twitter Login

--- backend/appengine/routes/login/home.py
+++ backend/appengine/routes/login/home.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from google.appengine.api import users
+
 from gaecookie.decorator import no_csrf
 from gaepermission import facade
 from gaepermission.decorator import login_not_required
 from tekton import router
 from config.template_middleware import TemplateResponse
-from routes.login import google, facebook
+from routes.login import google, facebook, twitter
 from routes.login.passwordless import send_email
 
 
@@ -17,5 +18,6 @@ def index(ret_path='/'):
     dct = {'login_google_path': users.create_login_url(g_path),
            'login_passwordless_path': router.to_path(send_email, ret_path=ret_path),
            'login_facebook_path': router.to_path(facebook.index, ret_path=ret_path),
+           'login_twitter_path': router.to_path(twitter.index),
            'faceapp': facade.get_facebook_app_data().execute().result}
     return TemplateResponse(dct)
